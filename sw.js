b9b1c43d46b90fe76dd8b15f501aba9ae5a8f6f3
yummymeenny-cyber <yummy.meenny@gmail.com>
2026-09-04 17:09:11 +0700

    Add CubeScope AR web app: sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='cubescope-ar-v1';
+const ASSETS=['./','./index.html','./style.css','./game.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
